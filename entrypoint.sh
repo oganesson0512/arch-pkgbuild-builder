cd5c01f027216e097cfa2738cb8a91f7f17baf87
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-10-31 21:24:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,3 +84,6 @@ esac
 
 sudo setfacl --restore=/tmp/arch-pkgbuild-builder-permissions.bak
 done
+case $target in
+    repo-add)
+        repo-add coolapk-linux/x86_64/coolapk-linux.db.tar.gz pkgbuild/*/*.zst
