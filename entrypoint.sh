77c71c5ed3459eef0fcc0f466fde0b3e972ec1be
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-10-31 14:11:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 target=$1
 dictionary=$2
 command=$3
-for pkg in `ls -AF $dictionary |grep '/$' | tr -d '/'`
+for pkg in `cat need-update`
 do
 pkgname="$dictionary"/"$pkg"
 # assumes that package files are in a subdirectory
