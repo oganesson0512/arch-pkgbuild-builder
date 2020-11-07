a28673a38911cfa82b0ede823178c19de405215d
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-11-07 21:46:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ case $target in
         do
           cd coolapk-linux
           pkgname=$(ls ../pkgbuild/${pkg}/*.zst|awk -F\/ '{print $4}')
-          mv ../pkgbuild/${pkg}/${pkgname} coolapk-linux/x86_64/
-          repo-add coolapk-linux/x86_64/coolapk-linux.db.tar.gz coolapk-linux/x86_64/${pkgname}
+          sudo mv ../pkgbuild/${pkg}/${pkgname} coolapk-linux/x86_64/
+          sudo repo-add coolapk-linux/x86_64/coolapk-linux.db.tar.gz coolapk-linux/x86_64/${pkgname}
           done
           ;;
     *)
